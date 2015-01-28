7dc47267d96c5ef9cae8ef90d2ccbbe63bfd6f63
Darron Froese <darron@froese.org>
2015-01-27 18:38:26 -0700

    Make chef-sugar available to Travis.

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer       'Darron Froese'
 maintainer_email 'darron@froese.org'
 license          'Apache 2.0'
 description      'Installs/configures docker'
-version          '0.16.0'
+version          '1.0.0'
 recipe           'docker::default', 'Installs/configures docker'
 
 depends 'apt'
